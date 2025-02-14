f523c171125e3108fcb0c1042bca165c4968e937
natelwhite <natelwhite01@gmail.com>
2025-02-14 02:04:32 -0500

    Testing 3d shader

--- src/main.cpp
+++ src/main.cpp
@@ -5,84 +5,61 @@
 Context* Context::self = 0;
 
 int main() {
-	// calculate tangent points
-	const int rad { 1 };
-	const int num_tangent_points { 128 };
-	SDL_FPoint tangent_points[num_tangent_points];
-	SDL_FColor tangent_colors[num_tangent_points];
-	for (int i = 0; i < num_tangent_points; i++) {
-		SDL_FPoint result;
-		auto calcTangent = [](const float &degrees, const float &radius) -> SDL_FPoint {
-			const float PI { 3.14159265 };
-			return SDL_FPoint {
-				static_cast<float>(SDL_cos(degrees * PI / 180.0) * radius),
-				static_cast<float>(SDL_sin(degrees * PI / 180.0) * radius)
-			};
-		};
-		auto calcColor = [](const float &deg) -> SDL_FColor {
-			return SDL_FColor {deg * 255, deg * 255, deg * 255, 255};
-		};
+	const size_t VERTEX_COUNT { 24 }, INDEX_COUNT { 36 };
+	const PositionVertex vertices[VERTEX_COUNT] = {
+		{ -10, -10, -10 },
+		{ 10, -10, -10 },
+		{ 10, 10, -10 },
+		{ -10, 10, -10 },
 
-		float deg;
-		if (i == 0) {
-			deg = 0;
-		} else {
-			deg = (360.0 / num_tangent_points) * i;
-		}
-		tangent_points[i] = calcTangent(deg, rad);
-		tangent_colors[i] = calcColor(deg / 360);
-	}
+		{ -10, -10, 10 },
+		{ 10, -10, 10 },
+		{ 10, 10, 10 },
+		{ -10, 10, 10 },
+
+		{ -10, -10, -10 },
+		{ -10, 10, -10 },
+		{ -10, 10, 10 },
+		{ -10, -10, 10 },
+
+		{ 10, -10, -10 },
+		{ 10, 10, -10 },
+		{ 10, 10, 10 },
+		{ 10, -10, 10 },
+
+		{ -10, -10, -10 },
+		{ -10, -10, 10 },
+		{ 10, -10, 10 },
+		{ 10, -10, -10 },
+
+		{ -10, 10, -10 },
+		{ -10, 10, 10 },
+		{ 10, 10, 10 },
+		{ 10, 10, -10 },
+	};
+	const Uint16 indices[INDEX_COUNT] = {
+		 0,  1,  2,  0,  2,  3,
+		 6,  5,  4,  7,  6,  4,
+		 8,  9, 10,  8, 10, 11,
+		14, 13, 12, 15, 14, 12,
+		16, 17, 18, 16, 18, 19,
+		22, 21, 20, 23, 22, 20
+	};
 
 	Renderer renderer {640, 480};
-	const size_t VERTEX_COUNT { num_tangent_points * 3 };
-	const char *vert_shader { "PositionColor.vert" }, *frag_shader { "SolidColor.frag" };
-	VertexBufferMat mat {vert_shader, frag_shader, VERTEX_COUNT};
+	const char *vert_shader { "Skybox.vert" }, *frag_shader { "Skybox.frag" };
+	ThreeDMat mat {vert_shader, frag_shader, VERTEX_COUNT, INDEX_COUNT};
 
 	// push vertices
 	SDL_FPoint origin {0, 0};
-	PositionColorVertex *buffer { mat.open() };
-	int buffer_index {};
-	PositionColorVertex vert;
-	for (int i = 0; i < num_tangent_points; i++) {
-		vert = {
-			tangent_points[i].x,
-			tangent_points[i].y,
-			0,
-			static_cast<Uint8>(tangent_colors[i].r),
-			static_cast<Uint8>(tangent_colors[i].g),
-			static_cast<Uint8>(tangent_colors[i].b),
-			static_cast<Uint8>(tangent_colors[i].a),
-		};
-		buffer[buffer_index++] = vert;
-
-		vert = { origin.x, origin.y, 0, 0, 0, 0, 255 };
-		buffer[buffer_index++] = vert;
-
-		if (i + 1 < num_tangent_points) {
-			vert = {
-				tangent_points[i + 1].x,
-				tangent_points[i + 1].y,
-				0,
-				static_cast<Uint8>(tangent_colors[i + 1].r),
-				static_cast<Uint8>(tangent_colors[i + 1].g),
-				static_cast<Uint8>(tangent_colors[i + 1].b),
-				static_cast<Uint8>(tangent_colors[i + 1].a),
-			};
-		} else {
-			vert = {
-				tangent_points[0].x,
-				tangent_points[0].y,
-				0,
-				255,
-				255,
-				255,
-				255,
-			};
-		}
-		buffer[buffer_index++] = vert;
-	}
-	mat.upload();
-	buffer = nullptr;
+	PositionVertex *vert_buffer { mat.vertBuffer()->open() };
+	SDL_memcpy(vert_buffer, vertices, VERTEX_COUNT);
+	mat.vertBuffer()->upload();
+	vert_buffer = nullptr;
+	Uint16 *index_buffer { mat.indexBuffer()->open() };
+	SDL_memcpy(index_buffer, indices, INDEX_COUNT);
+	mat.indexBuffer()->upload();
+	index_buffer = nullptr;
 
 	// main loop
 	bool quit = false;
@@ -101,6 +78,47 @@ int main() {
 				case SDLK_R:
 					mat.refresh();
 					break;
+				case SDLK_W: {
+					ContextData ctx { Context::get()->data() };
+					ctx.camera_pos.at(1) += 1;
+					Context::get()->set(ctx);
+					break;
+				}
+				case SDLK_A: {
+					ContextData ctx { Context::get()->data() };
+					Vector3 cam_pos { ctx.camera_pos };
+					ctx.camera_pos.at(0) -= 1;
+					Context::get()->set(ctx);
+					break;
+				}
+				case SDLK_S: {
+					ContextData ctx { Context::get()->data() };
+					Vector3 cam_pos { ctx.camera_pos };
+					ctx.camera_pos.at(1) -= 1;
+					Context::get()->set(ctx);
+					break;
+				}
+				case SDLK_D: {
+					ContextData ctx { Context::get()->data() };
+					Vector3 cam_pos { ctx.camera_pos };
+					ctx.camera_pos.at(0) += 1;
+					Context::get()->set(ctx);
+					break;
+				}
+				case SDLK_Z: {
+					ContextData ctx { Context::get()->data() };
+					Vector3 cam_pos { ctx.camera_pos };
+					ctx.camera_pos.at(2) += 1;
+					Context::get()->set(ctx);
+					break;
+				}
+				case SDLK_X: {
+					ContextData ctx { Context::get()->data() };
+					Vector3 cam_pos { ctx.camera_pos };
+					ctx.camera_pos.at(2) -= 1;
+					Context::get()->set(ctx);
+					break;
+				}
 				}
 			}
 		}
