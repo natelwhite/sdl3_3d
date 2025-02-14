16db147efdd23024d7802daa4a780270e706eb3c
natelwhite <natelwhite01@gmail.com>
2025-02-13 22:48:02 -0500

    Revert "Create structs and methods necessary for 3d -> screen space projection"
    This reverts commit 81f13599fe023c12a21419b3cf79ba5d06446e17.

--- include/Materials.hpp
+++ include/Materials.hpp
@@ -8,6 +8,16 @@ typedef struct PositionColorVertex {
 	Uint8 r, g, b, a;
 } PositionColorVertex;
 
+struct Vector3 : std::array<float, 3> {
+	Vector3 normalize() const;
+	float dot(const Vector3 &other) const;
+	Vector3 cross(const Vector3 &other) const;
+};
+struct Vector4 : std::array<float, 4> {};
+struct Matrix4x4 : std::array<Vector4, 4> {
+	Matrix4x4 operator * (const Matrix4x4 &other);
+};
+
 class Material {
 	public:
 		Material(const char *t_vert_file, const char *t_frag_file);
@@ -20,6 +30,8 @@ class Material {
 		SDL_GPUGraphicsPipeline *m_pipeline;
 		SDL_GPUGraphicsPipelineCreateInfo m_pipeline_info;
 
+		const Matrix4x4 getFov(const float &fov, const float &aspect, const float &near, const float &far);
+		const Matrix4x4 getLookAt(const Vector3 &camera_pos, const Vector3 &camera_target, const Vector3 &camera_up);
 		SDL_GPUShader* loadShader(const char *filename, Uint32 num_samplers, Uint32 num_uniform_buffers, Uint32 num_storag_buffers, Uint32 num_storage_textures);
 };
 
